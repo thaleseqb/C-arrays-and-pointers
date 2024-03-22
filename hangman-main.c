0564f761b49da6dcde5516de5de2bdf616df4b01
Thales <thalesquadros2@gmail.com>
2024-03-22 15:56:16 -0300

    still studying arrays with hangman game

--- hangman-main.c
+++ hangman-main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 
 
 int main() {
@@ -11,20 +11,23 @@ int main() {
     printf("-------------------------\n");
 
     char secret_word[number_of_letters];
-    
+
     sprintf(secret_word, "melancia");
 
     int won = 0;
-    int hanged = 1; 
+    int hanged = 0; 
 
     do {
+        // printf("%c", scanf("%c", &attempt));
+
+        for (int idx = 0; idx < strlen(secret_word); idx++) {
+            printf("_ ");
+        }
+
+        printf("\n");
 
         char attempt;
-        scanf("%c", &attempt);
-
-
+        scanf(" %c", &attempt);
 
     } while (!won && !hanged);
-     
-
 }
