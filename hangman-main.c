f7ef81a87e0b626ee6f95b595de7c47bea6c9cd3
thaleseqb <thalesquadros2@gmail.com>
2024-03-23 11:30:55 -0300

    introduction in looping structures in C and boolean manipulation

--- hangman-main.c
+++ hangman-main.c
@@ -1,33 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 
-
 int main() {
 
-    int number_of_letters;
+    int attempt_number;
 
-    printf("Insert the number of letters in your word\n");
-    scanf("%d", &number_of_letters);
-    printf("-------------------------\n");
+    printf("Insert the number of attempts\n");
+    scanf("%d", &attempt_number);
+    printf("----------------------\n");
 
-    char secret_word[number_of_letters];
-
-    sprintf(secret_word, "melancia");
+    char secret_word[attempt_number];
+    sprintf(secret_word, "MELANCIA");
 
     int won = 0;
-    int hanged = 0; 
+    int hanged = 0;
+
+    char kicks[26];
+    int attempts = 0;
 
     do {
-        // printf("%c", scanf("%c", &attempt));
 
         for (int idx = 0; idx < strlen(secret_word); idx++) {
-            printf("_ ");
+
+            int found = 0;
+            
+            for (int jidx = 0; jidx < attempts; jidx++) {
+                if (kicks[jidx] == secret_word[idx]) {
+                    found = 1;
+                    break;
+                }
+            }
+
+                if (found) {
+                    printf("%c ", secret_word[idx]);
+                } else {
+                    printf("_ ");
+                }
         }
 
         printf("\n");
 
         char attempt;
+        printf("Qual a letra ?\n");
         scanf(" %c", &attempt);
+
+        kicks[attempts] = attempt;
+        attempts++;
 
     } while (!won && !hanged);
 }
